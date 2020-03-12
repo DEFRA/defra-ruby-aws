046ef675703dcc9b79da61b49fc43c3a12de7729
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-03-12 10:11:41 +0000

    Add spec filter to simplecov
    We still get 100% reported, but if you look at the simplecov detail now it no longer lists the spec files.

--- spec/support/simplecov.rb
+++ spec/support/simplecov.rb
@@ -7,6 +7,10 @@ require "simplecov"
 # This gives us the most accurate assessment of our unit test coverage
 # https://github.com/colszowka/simplecov#getting-started
 SimpleCov.start do
+  # We filter the spec folder, mainly to ensure that any dummy apps don't get
+  # included in the coverage report. However our intent is that nothing in the
+  # spec folder should be included
+  add_filter "/spec/"
   # The version file is simply just that, so we do not feel the need to ensure
   # we have a test for it
   add_filter "lib/defra_ruby/aws/version"
