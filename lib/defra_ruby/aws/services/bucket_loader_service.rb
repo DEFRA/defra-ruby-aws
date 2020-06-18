1c624af76970f6611b4577e5326678550e8e0be4
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-06-18 23:00:33 +0100

    Switch encryption type

--- lib/defra_ruby/aws/services/bucket_loader_service.rb
+++ lib/defra_ruby/aws/services/bucket_loader_service.rb
@@ -24,7 +24,7 @@ module DefraRuby
 
       def response_exe
         lambda do
-          s3_bucket.object(File.basename(file.path)).upload_file(file.path, server_side_encryption: :AES256)
+          s3_bucket.object(File.basename(file.path)).upload_file(file.path, server_side_encryption: "aws:kms")
         end
       end
     end
