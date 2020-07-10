6ef18f84d78fe1bdc7aa30e76325c1be00c96cf0
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-07-10 12:48:14 +0100

    Add some tests for region
    Doing this ahead of adding some for the encryption type. Made sense if I felt the need to cover encryption type we should also be covering region.

--- spec/lib/defra_ruby/aws/bucket_spec.rb
+++ spec/lib/defra_ruby/aws/bucket_spec.rb
@@ -7,6 +7,8 @@ module DefraRuby
     RSpec.describe Bucket do
       subject(:bucket) { described_class.new(configs) }
 
+      let(:credentials) { { access_key_id: "access_key", secret_access_key: "secret" } }
+
       describe "#initialize" do
         context "when a name configuration is missing" do
           let(:configs) do
@@ -63,16 +65,57 @@ module DefraRuby
             expect { bucket }.to raise_error("DefraRuby::Aws buckets configurations: missing secret access key for bucket foo")
           end
         end
+
+        context "when 'region' is not set" do
+          context "because it has not been added to the config" do
+            let(:configs) do
+              {
+                name: "foo",
+                credentials: credentials
+              }
+            end
+
+            it "defaults the region to 'eu-west-1'" do
+              expect(bucket.region).to eq("eu-west-1")
+            end
+          end
+
+          context "because its value is an empty string" do
+            let(:configs) do
+              {
+                name: "foo",
+                credentials: credentials,
+                region: ""
+              }
+            end
+
+            it "defaults the region to 'eu-west-1'" do
+              expect(bucket.region).to eq("eu-west-1")
+            end
+          end
+        end
+
+        context "when 'region' is set" do
+          let(:region) { "eu-west-2" }
+          let(:configs) do
+            {
+              name: "foo",
+              credentials: credentials,
+              region: region
+            }
+          end
+
+          it "sets the region to match" do
+            expect(bucket.region).to eq(region)
+          end
+        end
       end
 
       describe "#load" do
         let(:configs) do
           {
             name: "foo",
-            credentials: {
-              secret_access_key: "secret",
-              access_key_id: "access_key"
-            }
+            credentials: credentials
           }
         end
 
